d8c201910b5d244f4d57df1cf2b06bc4b2216c2f
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-07-25 23:40:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from jikanpy import Jikan
 import signal
 import sys
-api_id = int("3845818")
+api_id = 3845818
 api_hash = "95937bcf6bc0938f263fc7ad96959c6d"
 bot_token = "5210009358:AAESvuzGgAhRITt0BZxgrMjnRqlq2yDf18Q"
 app = Client("my_anime_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
@@ -23,34 +23,6 @@ def handle_message(client, message):
       app.send_message(message.chat.id, anime_info)  # Send the anime information as a reply
    except IndexError:
       app.send_message(message.chat.id, "Anime not found.")  # Send a message if the anime is not found
-from pyrogram import Client
-from jikanpy import Jikan
-import signal
-import sys
-
-api_id = "YOUR_API_ID"
-api_hash = "YOUR_API_HASH"
-bot_token = "YOUR_BOT_TOKEN"
-
-app = Client("my_anime_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-
-@app.on_message()
-def handle_message(client, message):
-    anime_title = message.text
-
-    try:
-        jikan = Jikan()
-        anime = jikan.search("anime", anime_title)["results"][0]
-        anime_info = f"Title: {anime['title']}\n"
-        anime_info += f"Type: {anime['type']}\n"
-        anime_info += f"Episodes: {anime['episodes']}\n"
-        anime_info += f"Score: {anime['score']}\n"
-        anime_info += f"Synopsis: {anime['synopsis']}\n"
-
-        client.send_message(message.chat.id, anime_info)
-    except IndexError:
-        client.send_message(message.chat.id, "Anime not found.")
-
 def stop_client(signal, frame):
    app.stop()
    sys.exit(0)
