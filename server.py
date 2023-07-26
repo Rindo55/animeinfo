6d4111783946d7667222d0495494f8865988373a
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-07-26 17:31:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,14 +8,12 @@ api_id = 3845818
 api_hash = "95937bcf6bc0938f263fc7ad96959c6d"
 bot_token = "5210009358:AAESvuzGgAhRITt0BZxgrMjnRqlq2yDf18Q"
 app = Client("anime_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-
-   
 def get_anime_info(anime_title):
     url = f"https://api.jikan.moe/v4/anime?q={anime_title}"
     response = requests.get(url)
     data = response.json()
-
-    if data:
+   
+    if data and len(data) > 0:
         anime = data[0]
         anime_info = f"Title: {anime['title']['romaji']}\n"
         anime_info += f"Type: {anime['type']}\n"
