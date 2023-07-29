7fa789a0927fe9a44c7634262250ef5bd4469542
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-07-29 22:51:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -315,7 +315,7 @@ async def get_anilist_data(name):
 @app.on_message(filters.command("anilist"))
 def handle_message(client, message):
     name = " ".join(message.command[1:])
-    result = get_anilist_data(name)
+    result = await get_anilist_data(name)
     img, caption = result
     main = client.send_photo(message.chat.id,photo=img,caption=caption)
 app.start()
