645de98c0cbf4fbc9d537ebe60e49906474c3ad3
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-08-04 20:31:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -360,10 +360,11 @@ async def get_anilist_data(title):
       malrank = mal['rank']
       malpopularity = mal['popularity']
       synopsi = mal['synopsis']
-    ges = f"https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Synopsis&author_name=AnimeArchivist&content=[{malrank}]&return_content=true"
+      synx = f"[{synopsi}]"
+    ges = f"https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Synopsis&author_name=AnimeArchivist&content={synx}&return_content=true"
     req = requests.get(ges)
     rem = req.json()
-    syn = rem['error']
+    syn = rem['result']['url]
     caption = atext.format(
       title2,
       title1,
