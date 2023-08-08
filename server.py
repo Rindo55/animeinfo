646a069dd419618cfab9a9881eb3e08fcd6ea183
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-08-08 18:19:01 +0530

    Update server.py

--- server.py
+++ server.py
@@ -449,6 +449,7 @@ async def handle_message(client, message):
     if processing:
         tk = await message.reply_text("Your request is in queue.")
         await command_queue.put(message)
+        await asyncio.sleep(10)
         await tk.delete()
     else:
         await command_queue.put(message)
